2c7577556056bfde2ab0139cfa9999be0292d4c0
System Administrator <root@MacBookAir.lan>
2024-11-25 13:46:17 -0800

    DP-5 Done

---
+++ Problem1.py
@@ -0,0 +1,14 @@
+# Time Complexity = O(m*n*k) Space Complexity = O(n)
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: list[str]) -> bool:
+        dp = [True] + [False] * len(s)
+
+        for i in range(1, len(s) + 1):
+            for w in wordDict:
+                start = i - len(w)
+                if start >= 0 and dp[start] and s[start:i] == w:
+                    dp[i] = True
+                    break
+
+        return dp[-1]
